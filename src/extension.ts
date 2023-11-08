0e5ec88bdc56137c159b7a3934b8cebc0eb7bc0d
aenewsss <aenamartinelli@gmail.com>
2023-11-08 18:00:41 -0300

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,60 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "edit-text-aenewsss" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('edit-text-aenewsss.helloWorld', () => {
+
+		// vscode.window.showInputBox()
+		const editor = vscode.window.activeTextEditor;
+
+		if (editor) {
+			let { selection } = editor;
+
+			if (selection && !selection.isEmpty) {
+				const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+				const highlighted = editor.document.getText(selectionRange);
+
+				const quickPick = vscode.window.createQuickPick()
+
+				quickPick.items = [{
+					label: 'Capitalize',
+				},
+				{
+					label: 'First letter capitalize',
+				},
+				{
+					label: 'CAPS LOCK',
+				},
+				{
+					label: 'Lowercase',
+				}
+				]
+
+				quickPick.show()
+
+				highlighted.toLocaleUpperCase()
+				vscode.window.showInformationMessage(highlighted);
+			} else vscode.window.showErrorMessage('You must select a text')
+		}
+
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Modified Text!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() { }
